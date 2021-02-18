d53be8e70f3d2b1d1d68364eeecc92fcbd892701
Everton Cabral <evertoncab@gmail.com>
2021-02-18 02:14:12 -0300

    testando v-for e v-if

--- vue-cli-terminal-exercicio10/src/App.vue
+++ vue-cli-terminal-exercicio10/src/App.vue
@@ -1,11 +1,19 @@
 <template>
   <div id="app">
     <ul>
-      <li v-for="(item, index ) in toDos" :key="index">
-        {{ item }} e {{index}}
-        <li v-for="(item, index ) in toDos2" :key="index">
-        {{ item }} e {{index}}
+      <li v-for="(item, index) in toDos" :key="index">
+        {{ item }} e {{ index }}
       </li>
+
+      <li v-for="(item, index) in toDos2" :key="index">
+        {{ item }} e {{ index }}
+      </li>
+      <template v-if="toDos3.lenght > 0">
+        <li v-for="(toDos3, index) in filteredToDos3" :key="index">
+          {{ toDos3 }} e {{ index }}
+        </li>
+      </template>
+      <li v-else> Nenhum item</li>
     </ul>
   </div>
 </template>
@@ -18,11 +26,22 @@ export default {
     return {
       toDos: ["item 1", "item 2", "item 3"],
       toDos2: {
-        item1: 'item 1',
-        item2: 'item 2',
-        item3: 'item 3',
+        item1: "item 1",
+        item2: "item 2",
+        item3: "item 3",
       },
+      toDos3: [
+        { label: "ToDO 1", isCompleted: false },
+        { label: "ToDO 2", isCompleted: false },
+        { label: "ToDO 3", isCompleted: true },
+        { label: "ToDO 4", isCompleted: true },
+      ],
     };
+  },
+  computed: {
+    filteredToDos3() {
+      return this.toDos3.filter((toDo3) => !toDo3.isCompleted);
+    },
   },
 };
 </script>
