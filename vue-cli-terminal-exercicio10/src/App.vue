da776f6fe489246be28ffb32c358fae5f9152bc3
Everton Cabral <evertoncab@gmail.com>
2021-02-18 01:47:57 -0300

    iterando lista de itens [] e {}

--- vue-cli-terminal-exercicio10/src/App.vue
+++ vue-cli-terminal-exercicio10/src/App.vue
@@ -1,19 +1,37 @@
 <template>
   <div id="app">
-  
+    <ul>
+      <li v-for="(item, index ) in toDos" :key="index">
+        {{ item }} e {{index}}
+        <li v-for="(item, index ) in toDos2" :key="index">
+        {{ item }} e {{index}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  components: {
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      toDos: ["item 1", "item 2", "item 3"],
+      toDos2: {
+        item1: 'item 1',
+        item2: 'item 2',
+        item3: 'item 3',
+      },
+    };
+  },
+};
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
